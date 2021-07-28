473c9f0a302f4c5eef9b1025e02fabe97a8cf071
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-07-28 22:54:38 +0800

    project init

---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <footer class="navbar navbar-expand-lg navbar-light bg-primary">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 d-flex justify-content-between">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">首頁</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">系列鏡框</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">門市據點</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">部落格</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">常見問題</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><img src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/week3/icon-fb.svg" alt=""></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><img src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/week3/icon-ig.svg" alt=""></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><img src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/week3/icon-line.svg" alt=""></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </footer>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="navbar navbar-expand bg-primary flex-wrap d-flex">
+    <div class="container navbar-container" style="padding-left: 0px; padding-right: 0px;">
+        <h1 class="logo"></h1>
+        <div class="collapse navbar-collapse flex-wrap d-flex justify-content-end">
+            <ul class="navbar-nav item-group">
+                <li class="nav-item">
+                    <a class="nav-link text-center text-white" aria-current="page" href="#">系列鏡框</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-center text-white" aria-current="page" href="#">門市據點</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-center text-white" aria-current="page" href="#">部落格</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-center text-white" aria-current="page" href="#">常見問題</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  </nav>
+</template>
